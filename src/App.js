269ca87a1edb25b9bf2b6128bf6db89ad2d9a10c
Samuel Selvarajah <samueljselvarajah@gmail.com>
2020-02-06 21:51:38 -0500

    changed date filter so its input and uses bootstrap date filter. also added a submit button. now just need to add api functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
 import React from 'react';
 import './App.css';
-import CustomDatePicker from './CustomDatePicker';
 
-function App() {
+class App extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageDate: ''
+
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = event => {
+    this.setState({ imageDate: event.target.value });
+  };
+
+render(){
   return (
-      <div class="row">
-        <div class="col-md-6 mx-auto">
-          <div class="form-group mb-4">
-            <div id="dateform" class="input-group p-0 shadow-sm">
-              <CustomDatePicker> </CustomDatePicker>
-              <div class ="input-group-append"><span class="input-group-text px-4"><i class="fa fa-clock-o"></i></span></div>
+      <div className="row">
+        <div className="col-md-6 mx-auto">
+          <div className="form-group mb-4">
+
+
+            <div className="date input-group p-0 shadow-sm" data-provide="datepicker">
+              <input type="text"
+                value = {this.state.imageDate}
+                onChange = {this.handleChange}
+                placeholder="Choose a date to retrieve EPI"
+                className="form-control py-4 px-4"
+                id="imageDate"/>
+              <div className ="input-group-append"><span className="input-group-text px-4"><i className="fa fa-clock-o"></i></span></div>
             </div>
+
+
+            <div className="text-center" id="findEPI">
+              <button type="button" className="btn btn-primary">Find EPI</button>
+            </div>
+
           </div>
         </div>
       </div>
-  );
+    );
+  }
 }
 
 export default App;
