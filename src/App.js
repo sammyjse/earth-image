4b8a905be218232d7ce5416593e7ccc47a532de5
Samuel Selvarajah <samueljselvarajah@gmail.com>
2020-02-06 22:53:37 -0500

    added a img template, just to get an idea of how the page would look

--- src/App.js
+++ src/App.js
@@ -7,35 +7,42 @@ class App extends React.Component{
     super(props);
     this.state = {
       imageDate: ''
-
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleChange = event => {
+  handleChange(event) {
     this.setState({ imageDate: event.target.value });
   };
 
+  handleClick(event) {
+    alert('A date was submitted for EPI: ' + this.state.imageDate);
+    event.preventDefault();
+  }
+
 render(){
   return (
+
       <div className="row">
         <div className="col-md-6 mx-auto">
           <div className="form-group mb-4">
-
-
-            <div className="date input-group p-0 shadow-sm" data-provide="datepicker">
-              <input type="text"
+          <p class="text-center text-white"> Choose a date for Earth Polychromatic Image </p>
+            <div className="date input-group p-0 shadow-sm">
+              <input
+                type="date"
                 value = {this.state.imageDate}
                 onChange = {this.handleChange}
-                placeholder="Choose a date to retrieve EPI"
                 className="form-control py-4 px-4"
                 id="imageDate"/>
               <div className ="input-group-append"><span className="input-group-text px-4"><i className="fa fa-clock-o"></i></span></div>
             </div>
-
-
             <div className="text-center" id="findEPI">
-              <button type="button" className="btn btn-primary">Find EPI</button>
+              <button onClick={this.handleClick} type="button" className="btn btn-primary">Find EPI</button>
+            </div>
+
+            <div className="text-center" id="earthImage">
+              <img src="https://epic.gsfc.nasa.gov/archive/natural/2015/10/31/png/epic_1b_20151031074844.png" class="img-thumbnail"/>
             </div>
 
           </div>
